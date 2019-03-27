d8a582e718ba9b69a9f3864c3ea132d704599bb0
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-27 22:33:55 +0900

    add api root

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "cpp-httplib/httplib.h"
 
 int main() {
 
     std::cout << "C++ API is UP!" << std::endl;
+
+    httplib::Server svr;
+
+    svr.Get("/", [](const httplib::Request& req, httplib::Response& res) {
+        res.set_content("Hi! Welcome to C++ API!", "text/plain");
+    });
 
     return 0;
 }
