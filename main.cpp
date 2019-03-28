ab89874391183e5138e71e6921b5e2d981abb3da
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-28 18:57:15 +0900

    add /talks api

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 #include "cpp-httplib/httplib.h"
+#include <sstream>
+#include <string>
+#include <vector>
 
 int main() {
 
     std::cout << "C++ API is UP!" << std::endl;
+
+    std::vector<std::string> talks;
 
     httplib::Server svr;
 
     svr.Get("/", [](const httplib::Request& req, httplib::Response& res) {
         res.set_content("Hi! Welcome to C++ API!", "text/plain");
     });
+
+
+    svr.Get("/talks", [&](const httplib::Request& req, httplib::Response& res) {
+
+        std::ostringstream content;
+
+        for(auto&& t : talks)
+            content << "{ " << t.c_str() << " }," ;
+        
+        res.set_content(content.str(), "text/plain");
+    });
+
 
     return 0;
 }
